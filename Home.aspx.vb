cb964a946c20733eee34bf5649690c20f77fd1f3
Heidee07 <hei752.dee@gmail.com>
2026-02-18 17:34:19 -0600

    $ 1. Section Sobre Mí: queda terminado el apartado con sus respectivos requerimientos (título, descripción, badges y botón) | 2. Section Mis Proyectos: queda terminado el apartado con sus respectivos requerimientos (título, descripción, badges, botón, imagen y apartado de próximo proyecto)

--- Home.aspx.vb
+++ Home.aspx.vb
@@ -5,4 +5,22 @@
 
     End Sub
 
+    Protected Sub btnDownloadCV_Click(sender As Object, e As EventArgs) Handles btnDownloadCV.Click
+        Dim filePath As String = Server.MapPath("~/Files/CV-HeideeTorres.pdf")
+
+        Try
+            If System.IO.File.Exists(filePath) Then
+                Response.Clear()
+                Response.ContentType = "application/pdf"
+                Response.AddHeader("Content-Disposition", "attachment; filename=CV-HeideeTorres.pdf")
+                Response.TransmitFile(filePath)
+                Response.End()
+            End If
+        Catch ex As Exception
+            Debug.WriteLine("Error downloading file: " & ex.Message)
+        End Try
+
+    End Sub
+
+
 End Class
